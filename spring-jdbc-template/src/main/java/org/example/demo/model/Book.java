af3ab3ef235a3eb24e3002be5fddfdb87133d776
001ZQF744 <001ZQF744@LAPTOP-KLEV4V9B>
2021-06-28 02:45:55 -0700

    commit push 2

--- spring-jdbc-template/src/main/java/org/example/demo/model/Book.java
+++ spring-jdbc-template/src/main/java/org/example/demo/model/Book.java
@@ -1,22 +1,26 @@
-package org.example.demo.model;
-
+package com.example.demo.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Setter
 @Getter
-@ToString
+
 public class Book {
-	
+
 	private Integer bookId;
 	private String bookName;
 	private Double bookPrice;
-	
+
+	public void displayBook() {
+
+
+		System.out.println(bookId+"\t"+bookName+"\t"+bookPrice);
+		System.out.println("\n");
+	}
 
 }
--- spring-jdbc-template/src/main/java/org/example/demo/service/BookService.java
+++ spring-jdbc-template/src/main/java/org/example/demo/service/BookService.java
@@ -1,16 +1,11 @@
-package com.example.demo.dao;
-
+package org.example.demo.service;
 import java.sql.SQLException;
 import java.util.List;
 
 import com.example.demo.model.Book;
 
-public interface BookDao {
-	
-	//public Connection getMyConnection() throws SQLException;
-	
+public interface BookService {
 	public Book createBook(Book book) throws SQLException;
-	
 	public List<Book> displayAllBook() throws SQLException;
 
 }
