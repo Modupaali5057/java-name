e647028362331b3e284f90914af078abcac99bb4
001ZQF744 <001ZQF744@LAPTOP-KLEV4V9B>
2021-06-29 03:09:28 -0700

    spring java

---
+++ spring-hibernate-java-based/src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -0,0 +1,10 @@
+package com.example.demo.dao;
+import com.example.demo.dto.EmployeeDto;
+import com.example.demo.ui.EmployeeRequestModel;
+import com.example.demo.ui.EmployeeResponseModel;
+
+public interface EmployeeDao {
+	
+	public EmployeeResponseModel createEmployee(EmployeeDto employeeDetails);
+
+}
