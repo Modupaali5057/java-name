909817c45872650565ed9a66a53ead1154c01246
001ZQF744 <001ZQF744@LAPTOP-KLEV4V9B>
2021-06-29 05:20:46 -0700

    spring push 2

--- spring-hibernate-java-based/src/main/java/com/example/demo/dao/EmployeeDao.java
+++ spring-hibernate-java-based/src/main/java/com/example/demo/dao/EmployeeDao.java
@@ -1,4 +1,6 @@
 package com.example.demo.dao;
+import java.util.List;
+
 import com.example.demo.dto.EmployeeDto;
 import com.example.demo.ui.EmployeeRequestModel;
 import com.example.demo.ui.EmployeeResponseModel;
@@ -6,5 +8,9 @@ import com.example.demo.ui.EmployeeResponseModel;
 public interface EmployeeDao {
 	
 	public EmployeeResponseModel createEmployee(EmployeeDto employeeDetails);
+	
+	public List<EmployeeResponseModel> displayAllEmployee();
+	
+	public EmployeeResponseModel findByEmployeeId(String employeeId);
 
 }
