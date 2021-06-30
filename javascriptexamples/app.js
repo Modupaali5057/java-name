a9e951ddd3d3087cf2b7677f789e17b89582ca53
Modupaali5057 <85543819+Modupaali5057@users.noreply.github.com>
2021-06-30 05:47:59 -0700

    Add files via upload

---
+++ javascriptexamples/app.js
@@ -0,0 +1,15 @@
+let num1 = 314;
+let num2 = 314;
+ 
+if (num1 < num2)
+{
+    console.log('Num1 is less than Num2');
+}
+else if (num1 > num2)
+{
+    console.log("Num1 is greater than Num2");
+}
+else
+{
+    console.log("Default fallback code");
+}
---
+++ javascriptexamples/compare.js
@@ -0,0 +1,20 @@
+let var1 = 9;
+let var2 = 9;
+ 
+// Equal ===
+console.log(var1 === var2);
+ 
+// Not Equal !=
+console.log(var1 != var2);
+ 
+// Greater than >
+console.log(var1 > var2);
+ 
+// Less than <
+console.log(var1 < var2);
+ 
+// Greater than or equal to >=
+console.log(var1 >= var2);
+ 
+// Less than or equal to <=
+console.log(var1 <= var2);
