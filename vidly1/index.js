9d39a204aeb008bee9e7b0e32230efddc3858fe3
Modupaali5057 <85543819+Modupaali5057@users.noreply.github.com>
2021-07-05 04:28:35 -0700

    Add files via upload

---
+++ vidly1/index.js
@@ -0,0 +1,14 @@
+const vidly1Router=require('./routes/admin');
+const express = require('express');
+const app = express();
+app.use(express.json());
+app.use('/admin',vidly1Router);
+app.use((req,res,next)=>{
+  
+  res.status(400).send('<h3>bad request</h3>')
+})
+
+app.listen(3000,()=>{
+
+    console.log('server started on port number 3000');
+});
