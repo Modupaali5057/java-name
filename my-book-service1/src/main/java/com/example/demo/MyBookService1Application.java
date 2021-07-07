952774867b89f80db432a3490f0bab192d3fd187
Modupaali5057 <85543819+Modupaali5057@users.noreply.github.com>
2021-07-07 05:07:50 -0700

    Add files via upload

---
+++ my-book-service1/src/main/java/com/example/demo/MyBookService1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBookService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBookService1Application.class, args);
+	}
+
+}
