c4fb8633e45d962f66407591a4e6f92e0059a007
Modupaali5057 <85543819+Modupaali5057@users.noreply.github.com>
2021-07-12 05:22:10 -0700

    Add files via upload

---
+++ my-admin-server-1/src/main/java/com/example/demo/MyAdminServer1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAdminServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAdminServer1Application.class, args);
+	}
+
+}
